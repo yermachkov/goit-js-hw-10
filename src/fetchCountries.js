bba168d0ae5f64d2641b51aa51a4f16d6b73f9ef
Alexey Yermachkov <o.yermachkov@gmail.com>
2022-06-30 21:24:56 +0300

    adds all task

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+// Напиши функцію fetchCountries(name), яка робить HTTP-запит на ресурс name і повертає проміс з масивом країн - результатом запиту. Винеси її в окремий файл fetchCountries.js і зроби іменований експорт.
+import Notiflix from 'notiflix';
+
+export const fetchCountries = name => {
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.log("The country does not exist");
+    })
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
 import './css/styles.css';
-
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const form = document.querySelector('#search-box');
+const countriesList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+form.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
+
+function clearMarkup(element) {
+  return element.innerHTML = "";
+}
+
+function onSearchInput(event) {
+  const inputText = event.target.value.trim();
+
+  if (!inputText) {
+    clearMarkup(countriesList);
+    clearMarkup(countryInfo);
+    return;
+  }
+
+  fetchCountries(inputText).then(data => {
+    if (data.length > 10) {
+      Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+      clearMarkup(countriesList);
+      clearMarkup(countryInfo);
+      return;
+    } else if (data.length >= 2 && data.length <= 10) {
+      clearMarkup(countryInfo);
+      countriesList.innerHTML = createCountriesListMarkup(data);
+    }
+    else {
+      clearMarkup(countriesList);
+      countryInfo.innerHTML = createCountryInfoMarkup(data);
+      console.log(createCountryInfoMarkup(data));
+    };
+  })
+    .catch(error => {
+      clearMarkup(countriesList);
+      clearMarkup(countryInfo);
+      Notiflix.Notify.failure('Oops, there is no country with that name')
+  })
+}
+
+
+function createCountriesListMarkup(data) {
+  return data.map(({ flags, name }) =>
+    `<li>
+      <div class="countries-data-wrap">
+        <img src="${flags.svg}" alt="flag of ${name.official}" width="30">
+       ${name.official}
+      </div>
+    </li>`)
+    .join('');
+}
+  
+function createCountryInfoMarkup(data) {
+  return data.map(({ name, capital, population, flags, languages }) =>
+`
+  <div class="counry-header-wrap">
+  <img src="${flags.svg}" alt="flag of ${name.official}" width="30" height="30">
+  <h1>${name.official}</h1>
+  </div>
+
+  <p><b>Capital:</b> ${capital}</p>
+  <p><b>Population:</b> ${population}</p>
+  <p><b>Languages:</b> ${Object.values(languages)}</p>
+  `
+  ).join('');
+}
